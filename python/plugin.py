937257b80322087c7b19bc94bb5b3edfda41c893
Martin Andrews <GitHub@mdda.net>
2019-06-01 17:22:00 +0800

    Capture initial directory

--- python/plugin.py
+++ python/plugin.py
@@ -2,12 +2,18 @@ import json
 
 try:
   import vim
+  vim_launch_directory=vim.eval('g:vim_launch_directory')
 except:
   print("No vim module available outside vim")
-  pass
+  vim_launch_directory=None
 
 def insert_country():
   row, col = vim.current.window.cursor
   current_line = vim.current.buffer[row-1]
   new_line = current_line[:col] + _get_country() + current_line[col:]
   vim.current.buffer[row-1] = new_line
+
+if vim_launch_directory is not None:
+  pass
+  # Stuff to initialise plugin view here
+  
