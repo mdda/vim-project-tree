0efc8795bc313d8ce1707bfec83ed26296bf553c
Martin Andrews <GitHub@mdda.net>
2019-06-10 14:33:00 +0800

    Load in project tree structure

--- python/plugin.py
+++ python/plugin.py
@@ -8,16 +8,16 @@ except:
   print("No vim module available outside vim")
   vim_launch_directory=None
 
-config_tree_layout_file           = "project-tree-layout.ini"
-config_session_file               = "session.ini"
-
-def insert_country():
-  row, col = vim.current.window.cursor
-  current_line = vim.current.buffer[row-1]
-  new_line = current_line[:col] + _get_country() + current_line[col:]
-  vim.current.buffer[row-1] = new_line
+config_tree_layout_file = "project-tree-layout.ini"
+config_session_file     = "session.ini"
 
 sidebar_width=30
+
+#def insert_country():
+#  row, col = vim.current.window.cursor
+#  current_line = vim.current.buffer[row-1]
+#  new_line = current_line[:col] + _get_country() + current_line[col:]
+#  vim.current.buffer[row-1] = new_line
 
 if vim_launch_directory is not None:
   pass
@@ -63,7 +63,7 @@ if vim_launch_directory is not None:
     #sidebar_buffer[:] = ["Found directory"]
 
     session_path = os.path.join(vim_launch_directory, '.geany', config_session_file)
-    if os.path.isfile( session_path ):
+    if False and os.path.isfile( session_path ):
       # https://docs.python.org/3/library/configparser.html
       with open(session_path, 'r') as fin:
         config = configparser.ConfigParser()
@@ -95,3 +95,48 @@ if vim_launch_directory is not None:
             #doc.editor.goto_pos(at_line)
             sidebar_buffer.append( file_relative )
             #vim.command(f':edit {file_relative:s}') 
+
+    tree_path = os.path.join(vim_launch_directory, '.geany', config_tree_layout_file)
+    if os.path.isfile( tree_path ):
+      with open(tree_path, 'r') as fin:
+        config = configparser.ConfigParser()
+        config.readfp(fin)
+      
+      # (type, label, arr/filename)
+      # ('g', label, arr)
+      # ('f', label, filename)
+      tree=[ ('g', '-', []) ]
+      def _load_project_tree_branch(section, arr):
+        ## Create a nice dictionary of stuff from this section - each integer(sorted) can contain several entries
+        key_matcher = re.compile("(\d+)-?(\S*)")
+        d=dict()
+        for k,v in config.items(section):
+          #print("('%s', '%s')" % (k, v))
+          m = key_matcher.match(k)
+          if m:
+            order = int(m.group(1))
+            if order not in d:d[order]=dict()
+            d[order][m.group(2)] = v
+            
+        for k,vd in sorted(d.items()):  # Here, vd is dictionary of data about each 'k' item
+          if '' in vd: # This is a file (the default ending)
+            ## Just add the file to the tree where we are
+            #iter = model.append(parent, TreeViewRowFile(vd[''], label=vd.get('label', None)).row)
+            arr.append( ('f', vd[''], None ) ) 
+            # No need to store this 'iter' - can easily append after
+              
+          else:  # This is something special
+            if 'group' in vd:
+              group = vd['group']
+              ## Add the group to the tree, and recursively go after that section...
+              #iter = model.append(parent, TreeViewRowGroup(group, label=vd.get('label', None)).row)
+              arr.append( ('g', group, [] ) )
+              ### Descend with parent=iter
+              _load_project_tree_branch(section+'/'+group, arr[-1][2] )  # Add to group's array
+        
+      if config.has_section('.'):
+        #print("Found Root!")
+        #model.clear()
+        _load_project_tree_branch('.', tree[-1][2])
+        print(tree)
+
